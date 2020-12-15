bba43c467a38c26c1751b6fac9e457f5a84c80a6
ginpei <ginpei@gmail.com>
2020-12-15 10:24:21 -0800

    feat: 🎸 update notepad

--- src/components/EditorNotepad.tsx
+++ src/components/EditorNotepad.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
 import "./EditorNotepad.scss";
 
 export const EditorNotepad: React.FC = (props) => {
-  return <div className="EditorNotepad">EditorNotepad</div>;
+  const [text, setText] = useState("");
+
+  const onTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = event.currentTarget;
+    setText(value);
+  };
+
+  return (
+    <div className="EditorNotepad">
+      <textarea
+        className="EditorNotepad-text"
+        onChange={onTextChange}
+        value={text}
+      />
+    </div>
+  );
 };
