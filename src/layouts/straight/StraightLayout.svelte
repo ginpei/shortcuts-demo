5e49cea37c48a928874e5beb036c8fc3335a2ee6
ginpei <ginpei@ginpei.dev>
2022-07-02 22:20:23 -0700

    feat: 🎸 basic layouts

---
+++ src/layouts/straight/StraightLayout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let name = "";
+</script>
+
+<div class="{name} StraightLayout">
+  <div class="body">
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  .body {
+    box-sizing: border-box;
+    margin-inline: auto;
+    padding-inline: 1rem;
+    width: min(60rem, 100%);
+  }
+</style>
